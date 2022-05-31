2d15188d45ac6276af6edf103407f1ddcd511715
TaneriX07 <tanerivince@gmail.com>
2022-05-31 23:58:06 +0800

    Particles background working

---
+++ src/misc/tsParticlesOptions.ts
@@ -0,0 +1,27 @@
+export const tsParticlesOptions = {
+  particles: {
+    collisions: {
+      bounce: {
+        horizontal: {
+          random: {
+            enable: true,
+          },
+          value: {
+            min: 0.1,
+            max: 1,
+          },
+        },
+        vertical: {
+          random: {
+            enable: true,
+          },
+          value: {
+            min: 0.1,
+            max: 1,
+          },
+        },
+      },
+      enable: true,
+    },
+  },
+};
