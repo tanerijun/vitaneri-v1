3e1c63f6f688eadeda45b092a01af84061ffefd9
TaneriX07 <tanerivince@gmail.com>
2022-06-01 13:34:43 +0800

    Basic layout and move theme dir to src

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 import ParticlesBackground from '../src/components/ParticlesBackground';
+import { Box, Heading, Text } from '@chakra-ui/react';
 
 const Home: NextPage = () => {
   return (
@@ -17,6 +17,21 @@ const Home: NextPage = () => {
       </Head>
 
       {/* <ParticlesBackground /> */}
+      <Box height="100vh" width="100vw" bg="red.200">
+        <Box
+          position="absolute"
+          top="50%"
+          left="10%"
+          height={100}
+          bg="blue.200"
+        >
+          <Box>
+            <Heading>Vincent Taneri</Heading>
+            <Text>Software Developer</Text>
+          </Box>
+          <Box>Icons</Box>
+        </Box>
+      </Box>
     </>
   );
 };
