6d7c400ea83648a947c9205bf70a7a015b217296
TaneriX07 <tanerivince@gmail.com>
2022-06-01 23:08:29 +0800

    Add icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,24 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import ParticlesBackground from '../src/components/ParticlesBackground';
-import { Box, Heading, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  HStack,
+  Icon,
+  IconButton,
+  Link,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import {
+  RiGithubFill,
+  RiFacebookCircleFill,
+  RiTwitterFill,
+  RiLinkedinBoxFill,
+  RiMailFill,
+  RiMoreFill,
+} from 'react-icons/ri';
 
 const Home: NextPage = () => {
   return (
@@ -16,21 +33,41 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <ParticlesBackground />
+      {/* <ParticlesBackground /> */}
       <Box height="100vh" width="100vw" bg="red.200">
-        <Box
+        <VStack
           position="absolute"
           top="50%"
           left="10%"
-          height={100}
+          alignItems="flex-start"
+          spacing={8}
           bg="blue.200"
         >
-          <Box>
-            <Heading>Vincent Taneri</Heading>
-            <Text>Software Developer</Text>
-          </Box>
-          <Box>Icons</Box>
-        </Box>
+          <VStack alignItems="flex-start">
+            <Heading size={['xl', '2xl', '3xl', '4xl']}>Vincent Taneri</Heading>
+            <Text fontSize={['xs', 'sm', 'md']}>Software Developer</Text>
+          </VStack>
+          <HStack spacing={4}>
+            <Link href="#" target="_blank">
+              <Icon as={RiGithubFill} h={5} w={5} />
+            </Link>
+            <Link href="#" target="_blank">
+              <Icon as={RiLinkedinBoxFill} h={5} w={5} />
+            </Link>
+            <Link href="#" target="_blank">
+              <Icon as={RiFacebookCircleFill} h={5} w={5} />
+            </Link>
+            <Link href="#" target="_blank">
+              <Icon as={RiTwitterFill} h={5} w={5} />
+            </Link>
+            <Link href="#" target="_blank">
+              <Icon as={RiMailFill} h={5} w={5} />
+            </Link>
+            <Link href="#" target="_blank">
+              <Icon as={RiMoreFill} h={5} w={5} />
+            </Link>
+          </HStack>
+        </VStack>
       </Box>
     </>
   );
