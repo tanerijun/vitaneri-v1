6c19a93cadaca467098ce44c72eea66c38a7ab4f
TaneriX07 <tanerivince@gmail.com>
2022-06-01 18:50:45 +0800

    Remove unnecessary options from bouncySnow particles effect

--- src/misc/tsParticlesOptions.ts
+++ src/misc/tsParticlesOptions.ts
@@ -16,34 +16,16 @@ export const bouncySnow = {
   interactivity: {
     events: {
       onClick: {
-        enable: true,
+        enable: false,
         mode: 'repulse',
       },
       onHover: {
         enable: true,
         mode: 'repulse',
-        parallax: {
-          force: 100,
-        },
       },
       resize: false,
     },
     modes: {
-      bubble: {
-        distance: 400,
-        duration: 2,
-        opacity: 0.8,
-        size: 40,
-        divs: {
-          distance: 200,
-          duration: 0.4,
-          mix: false,
-          selectors: [],
-        },
-      },
-      grab: {
-        distance: 400,
-      },
       push: {
         quantity: 1,
       },
@@ -60,11 +42,6 @@ export const bouncySnow = {
           selectors: [],
         },
       },
-    },
-  },
-  motion: {
-    reduce: {
-      value: false,
     },
   },
   particles: {
@@ -91,64 +68,16 @@ export const bouncySnow = {
       },
       enable: true,
     },
-    color: {
-      animation: {
-        h: {
-          count: 20,
-          offset: 20,
-          speed: 20,
-          sync: false,
-        },
-        s: {
-          sync: false,
-        },
-        l: {
-          sync: false,
-        },
-      },
-    },
-    life: {
-      delay: {
-        sync: true,
-      },
-    },
-    links: {
-      color: {
-        value: '#ffffff',
-      },
-      distance: 150,
-    },
     move: {
       angle: {
         value: 45,
       },
-      attract: {
-        rotate: {
-          x: 600,
-          y: 1200,
-        },
-      },
       direction: 'bottom-left',
       drift: 0.01,
       enable: true,
-      path: {},
-      outModes: {
-        bottom: 'out',
-        left: 'out',
-        right: 'out',
-        top: 'out',
-      },
       speed: 6,
-      spin: {},
-      trail: {
-        length: 1,
-      },
     },
     number: {
-      density: {
-        area: 1000,
-        factor: 100,
-      },
       value: 80,
     },
     opacity: {
@@ -160,42 +89,11 @@ export const bouncySnow = {
         min: 0.4,
         max: 0.7,
       },
-      animation: {
-        speed: 3,
-        minimumValue: 0.1,
-      },
-    },
-    orbit: {
-      rotation: {
-        random: {
-          minimumValue: 1,
-        },
-      },
-    },
-    repulse: {
-      random: {
-        minimumValue: 50,
-      },
-      distance: 100,
-      factor: 100,
-      speed: 100,
-    },
-    roll: {
-      darken: {
-        value: 2,
-      },
-      enlighten: {
-        value: 200,
-      },
     },
     size: {
       value: {
         min: 2,
         max: 5,
-      },
-      animation: {
-        speed: 200,
-        minimumValue: 0.1,
       },
     },
     wobble: {
