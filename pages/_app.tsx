fa3bf4f97f442be8694fd270704768ea016cd0e8
TaneriX07 <tanerivince@gmail.com>
2022-06-01 23:15:38 +0800

    Remove focus indicator by ChakraUI when element is clicked but retain the effect when using keyboard navigation
    This is done with focus-visible, all implementation were done in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
 import '@fontsource/montserrat';
 import '@fontsource/inter';
-import '../styles/globals.css';
+
 import type { AppProps } from 'next/app';
+
+import 'focus-visible/dist/focus-visible';
+import { Global, css } from '@emotion/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../src/theme';
+
+const GlobalStyles = css`
+  /* This will hide the focus indicator if an element receive focus via mouse click, but the focus indicator will still show up on keyboard navigation */
+
+  .js-focus-visible :focus::not([data-focus-visible-added]) {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Global styles={GlobalStyles} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
