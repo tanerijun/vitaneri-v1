2924a0b81e5531569604e063b18dda37edae1497
TaneriX07 <tanerivince@gmail.com>
2022-06-03 00:08:57 +0800

    Add more todos

--- src/components/LinkIcon/index.tsx
+++ src/components/LinkIcon/index.tsx
@@ -7,6 +7,9 @@ interface LinkIconProp {
 }
 
 const LinkIcon = ({ href, icon }: LinkIconProp) => {
+  // TODO: If icon === RiMailFill (have to import this),
+  // the icon should act as a button that open a popover asking the user to contact me with mailto and a button to copy email
+
   // TODO: If icon === RiMoreFill (have to import this),
   // the icon should act as a button that open a modal/drawer to more info about me
 
