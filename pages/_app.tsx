11a139363667e4f67f7e39bcb1ae8aa913644297
TaneriX07 <tanerivince@gmail.com>
2022-06-03 15:15:51 +0800

    Remove focus visible
    The accessibility is actually not bad

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,19 +4,11 @@ import '@fontsource/source-sans-pro';
 
 import type { AppProps } from 'next/app';
 
-import 'focus-visible/dist/focus-visible';
 import { Global, css } from '@emotion/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../src/theme';
 
 const GlobalStyles = css`
-  /* This will hide the focus indicator if an element receive focus via mouse click, but the focus indicator will still show up on keyboard navigation */
-
-  .js-focus-visible :focus::not([data-focus-visible-added]) {
-    outline: none;
-    box-shadow: none;
-  }
-
   .glassmorphism {
     -webkit-backdrop-filter: blur(10px) saturate(125%); // Support for webkit
     backdrop-filter: blur(8px) saturate(125%);
