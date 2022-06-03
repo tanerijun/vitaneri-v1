cdcd54d2819413fe1c5df2f6c56f231b0311b43f
TaneriX07 <tanerivince@gmail.com>
2022-06-03 15:30:42 +0800

    Finish the Mail icon

--- src/components/LinkIcon/index.tsx
+++ src/components/LinkIcon/index.tsx
@@ -1,10 +1,8 @@
 import {
   Box,
   Button,
-  Center,
-  Icon,
   IconButton,
-  Link,
+  HStack,
   Tooltip,
   Popover,
   PopoverTrigger,
@@ -13,10 +11,8 @@ import {
   PopoverCloseButton,
   PopoverHeader,
   PopoverBody,
-  HStack,
+  useClipboard,
 } from '@chakra-ui/react';
-import { IconType } from 'react-icons/lib';
-import { ReactElement } from 'react';
 import {
   RiGithubFill,
   RiFacebookCircleFill,
@@ -26,61 +22,13 @@ import {
   RiMoreFill,
 } from 'react-icons/ri';
 
-interface LinkIconProp {
-  href?: string;
-  icon: ReactElement;
-  name: string;
-}
-
-// const LinkIcon = ({ href, icon, name }: LinkIconProp) => {
-//   if (name === 'Email') {
-//     // the icon should act as a button that open a popover asking the user to contact me with mailto and a button to copy email
-//     return (
-//       <Popover>
-//         <Tooltip label={name} fontSize="sm" bg="transparent" color="gray.200">
-//           <Center cursor="pointer" tabIndex={0}>
-//             <PopoverTrigger>
-//               <Icon
-//                 as={icon}
-//                 h={5}
-//                 w={5}
-//                 opacity={0.7}
-//                 _hover={{ opacity: 1 }}
-//               />
-//             </PopoverTrigger>
-//           </Center>
-//         </Tooltip>
-//         <PopoverContent>
-//           <PopoverArrow />
-//           <PopoverCloseButton />
-//           <PopoverHeader>Test</PopoverHeader>
-//           <PopoverBody>Test</PopoverBody>
-//         </PopoverContent>
-//       </Popover>
-//     );
-//   }
-
-// TODO: If icon === RiMoreFill (have to import this),
-// the icon should act as a button that open a modal/drawer to more info about me
-
-// return (
-//   <Tooltip label={name} fontSize="sm" bg="transparent" color="gray.200">
-//     <Link
-//       href={href}
-//       target="_blank"
-//       display="flex"
-//       alignItems="center"
-//       justifyContent="center"
-//     >
-//       <Icon as={icon} h={5} w={5} opacity={0.7} _hover={{ opacity: 1 }} />
-//     </Link>
-//   </Tooltip>
-// );
-
-//   return <IconButton aria-label={name} icon={icon} />;
-// };
-
 const EmailIcon = () => {
+  // This Icon display a popover giving user a choice whether to send an email using mailto or copy email address to clipboard
+
+  // Used to copy email to clipboard
+  const email = 'tanerivince@gmail.com';
+  const { hasCopied, onCopy } = useClipboard(email);
+
   return (
     <Popover>
       <Tooltip label="Email" fontSize="sm" bg="transparent" color="gray.200">
@@ -98,8 +46,15 @@ const EmailIcon = () => {
       <PopoverContent bg="blackAlpha.100">
         <PopoverArrow bg="blackAlpha.100" />
         <PopoverCloseButton />
-        <PopoverHeader>Confirmation!</PopoverHeader>
-        <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
+        <PopoverHeader>Contact Me</PopoverHeader>
+        <PopoverBody display="flex" gap={2}>
+          <Button as="a" href={`mailto:${email}`} target="_blank">
+            Send Email
+          </Button>
+          <Button flex="1" onClick={onCopy}>
+            {hasCopied ? 'Copied To Clipboard' : 'Copy Email Address'}
+          </Button>
+        </PopoverBody>
       </PopoverContent>
     </Popover>
   );
